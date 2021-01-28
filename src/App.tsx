91019e1ecb2bcb4f1ae2e54b61cc0f842dc639b0
Owen Hunt <ophunt2000@gmail.com>
2021-01-28 12:54:07 -0600

    Off to a good start

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Dropdown, Nav, Navbar, NavItem, NavLink } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from "./Home";
 import './App.css';
 
 function App() {
+  const [showNavPuzzlesDrop, setShowNavPuzzlesDrop] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World!
-        </p>
-      </header>
-    </div>
+    <Router>
+      <Navbar>
+        <Navbar.Brand href="/">MilkRei</Navbar.Brand>
+        <Nav activeKey="/">
+          <Dropdown as={NavItem} show={showNavPuzzlesDrop}
+            onMouseEnter={() => setShowNavPuzzlesDrop(true)}
+            onMouseLeave={() => setShowNavPuzzlesDrop(false)}>
+            <Dropdown.Toggle as={NavLink} href="/puzzles">Puzzles</Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="/puzzles/crosswords">Crosswords</Dropdown.Item>
+              <Dropdown.Item href="/puzzles/scrambles">Word Scrambles</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
+      </Navbar>
+      
+      <Switch>
+        <Route path="/puzzles">
+          {/* TODO */}
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Jumbotron } from "react-bootstrap";
+
+export default function Home() {
+    return (
+        <Jumbotron>
+            <h1>Welcome to MilkRei.com!</h1>
+        </Jumbotron>
+    )
+}
